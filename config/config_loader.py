cfe500a1bdbe14a6cfcaa06ba900023d5ad586dc
rajendra <rajendra_kulkarni@yahoo.com>
2026-06-21 22:43:22 +0530

    credentials seperated

--- config/config_loader.py
+++ config/config_loader.py
@@ -3,3 +3,7 @@ import yaml
 def load_config(path="config/config.yaml"):
     with open(path) as f:
         return yaml.safe_load(f)
+
+def load_credentials(path="config/credentials.yaml"):
+    with open(path) as f:
+        return yaml.safe_load(f)
